317236f1cbb9c3a1b392023ce16cc990cad16ba9
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-04-06 19:06:32 +0530

    [MODIFIED] : Calculated Employee Wages if Employee is Present

---
+++ src/com/bridgelabz/EmployeeWageComputation/UC2DailyEmployeeWage.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.EmployeeWageComputation;
+/*
+ * @author : Ashwini
+ * Calculate Daily Employee Wage
+ */
+public class UC2DailyEmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+			// Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+
+	}
+
+}
